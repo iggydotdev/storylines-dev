e0f590c29185fe8c9e1b345a5a561ce047d6f461
Iggy <77923839+iggydotdev@users.noreply.github.com>
2021-12-17 15:23:52 +1000

    Add files via upload

---
+++ src/components/tabs/tabs.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Tab from './tab.svelte';
+	import TabPanel from './tabPanel.svelte';	
+	
+	export let data;
+
+
+</script>
+
+<div>
+	<!-- {#each data as tab} -->
+		<Tab><button>X</button></Tab>
+	<!-- {/each} -->
+</div>
+
+<div>
+	<!-- {#each data as tab} -->
+		<TabPanel>
+			test
+		</TabPanel>
+	<!-- {/each} -->
+
+</div>
